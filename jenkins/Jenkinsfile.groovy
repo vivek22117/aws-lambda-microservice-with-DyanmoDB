78538bc63025280930b11667ef957033dba048b2
Vivek Mishra <vivekmishra22117@gmail.com>
2018-11-11 23:47:12 +0530

    UPdated lambda stack/Template

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 def createLambdaStack(String region, String stack, String vpc){
     sh "aws cloudformation --region ${region} validate-template --template-body file://aws-lambda-infra.json"
     sh "aws cloudformation --region ${region} create-stack --stack-name ${stack} --template-body \
-        file://aws-elb-for-ec2.json --parameters ParameterKey=VPCStackName,ParameterValue=${vpc}"
+        file://aws-lambda-infra.json --parameters ParameterKey=VPCStackName,ParameterValue=${vpc}"
     sh "aws cloudformation --region ${region} wait stack-create-complete --stack-name ${stack}"
     sh "aws cloudformation --region ${region} describe-stack-events --stack-name ${stack} \
         --query 'StackEvents[].[{Resource:LogicalResourceId,Status:ResourceStatus,Reason:ResourceStatusReason}]' \
