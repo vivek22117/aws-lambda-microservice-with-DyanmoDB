1c73fea0d699712155ff2308d3432a58a2753191
Vivek Mishra <vivekmishra22117@gmail.com>
2018-11-13 23:17:24 +0530

    UPdated lambda stack/Template

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -88,7 +88,7 @@ pipeline {
                                 --stack-name ${params.LAMBDASTACK} --query Stacks[0].StackStatus --output text", returnStdout: true)
                             if (status == 'DELETE_FAILED' || 'ROLLBACK_COMPLETE' || 'ROLLBACK_FAILED' || 'UPDATE_ROLLBACK_FAILED') {
                                 sh "aws cloudformation delete-stack --stack-name ${params.LAMBDASTACK} --region ${params.REGION}"
-                                sh 'echo Creating ASG group and configuration for web application....'
+                                sh 'echo Creating ASG group and configuration for web application after deleting....'
                                 createLambdaStack(region, stackName, vpc)
                             }
                             apply = true
