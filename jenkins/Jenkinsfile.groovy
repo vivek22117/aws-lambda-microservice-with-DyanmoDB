1944c1549693a79dfc9045301ca016eeb5530dc1
Vivek Mishra <vivekmishra22117@gmail.com>
2018-11-14 22:49:20 +0530

    Resolved infra issue in Lambda stack

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -1,4 +1,15 @@
-def createLambdaStack(String region, String stack, String vpc){
+def createLambdaStack(String region, String stack, String vpc, String s3){
+    sh "aws cloudformation --region ${region} validate-template --template-body file://aws-lambda-infra.json"
+    sh "aws cloudformation --region ${region} create-stack --stack-name ${stack} --template-body \
+        file://aws-lambda-infra.json --parameters ParameterKey=VPCStackName,ParameterValue=${vpc} \
+        ParameterKey=S3Stack,ParameterValue=${s3}"
+    sh "aws cloudformation --region ${region} wait stack-create-complete --stack-name ${stack}"
+    sh "aws cloudformation --region ${region} describe-stack-events --stack-name ${stack} \
+        --query 'StackEvents[].[{Resource:LogicalResourceId,Status:ResourceStatus,Reason:ResourceStatusReason}]' \
+        --output table"
+}
+
+def createS3Stack(String region, String stack, String vpc){
     sh "aws cloudformation --region ${region} validate-template --template-body file://aws-lambda-infra.json"
     sh "aws cloudformation --region ${region} create-stack --stack-name ${stack} --template-body \
         file://aws-lambda-infra.json --parameters ParameterKey=VPCStackName,ParameterValue=${vpc}"
@@ -21,6 +32,7 @@ pipeline {
         string(name: 'LAMBDASTACK', defaultValue: 'lambda-by-vivek', description:'worspace to use in Terraform')
         string(name: 'VPCSTACK', defaultValue: 'vpc-subnet-network-by-vivek', description:'worspace to use in Terraform')
         string(name: 'DBSTACK', defaultValue: 'rds-by-vivek', description:'RDS for Web Server')
+        string(name: 'S3STACK', defaultValue: 's3-by-vivek', description:'RDS for Web Server')
     }
     stages {
         stage('lambda-role'){
@@ -59,7 +71,7 @@ pipeline {
                         def apply = true
                         def policyArn = null
                         try {
-                           policyArn = sh(script: "aws iam create-policy --policy-name ${params.LAMBDAPOLICY} \
+                            policyArn = sh(script: "aws iam create-policy --policy-name ${params.LAMBDAPOLICY} \
                                 --policy-document file://aws-lambda-access-policy.json --query 'Arn' --output text", returnStdout: true)
                             apply = true
                         } catch(err){
@@ -76,6 +88,61 @@ pipeline {
                 }
             }
         }
+        stage('lambda-policy'){
+            steps {
+                dir('cloudformation/access/lambda-role-policy/'){
+                    script {
+                        def apply = true
+                        def policyArn = null
+                        try {
+                            policyArn = sh(script: "aws iam create-policy --policy-name ${params.LAMBDAPOLICY} \
+                                --policy-document file://aws-lambda-access-policy.json --query 'Arn' --output text", returnStdout: true)
+                            apply = true
+                        } catch(err){
+                            apply = false
+                            sh "echo Policy already exist updating policy...."
+                            policyArn = sh(script: "aws iam list-policies --query 'Policies[?PolicyName==`${params.LAMBDAPOLICY}`].Arn' \
+                                        --output text", returnStdout: true)
+                            sh "aws iam create-policy-version --policy-document file://aws-lambda-access-policy.json \
+                                --set-as-default --policy-arn $policyArn"
+                        }
+                        sh "aws iam attach-role-policy --role-name ${params.LAMBDAROLE} --policy-arn $policyArn"
+                        sh "echo Finished create/update successfully!"
+                    }
+                }
+            }
+        }
+        stage('Code-Deploy-Bucket'){
+            steps {
+                dir('cloudformation/lambda/'){
+                    script {
+                        def apply = true
+                        def status = null
+                        def region = params.REGION
+                        def stackName = params.S3STACK
+                        def vpc = params.VPCSTACK
+                        try {
+                            status = sh(script: "aws cloudformation describe-stacks --region ${params.REGION} \
+                                --stack-name ${params.S3STACK} --query Stacks[0].StackStatus --output text", returnStdout: true)
+                            if (status == 'DELETE_FAILED' || 'ROLLBACK_COMPLETE' || 'ROLLBACK_FAILED' || 'UPDATE_ROLLBACK_FAILED') {
+                                sh "aws cloudformation delete-stack --stack-name ${params.S3STACK} --region ${params.REGION}"
+                                sh 'echo Waiting for stack to delete....'
+                                sh "aws cloudformation --region ${params.REGION} wait stack-delete-complete --stack-name ${params.S3STACK}"
+                                sh 'echo Creating S3 bucket for code deployment after deleting....'
+                                createS3Stack(region, stackName, vpc)
+                            }
+                            apply = true
+                        } catch(err){
+                            apply = false
+                            sh 'echo Creating S3 Bucket for first time....'
+                            createS3Stack(region, stackName, vpc)
+                        }
+                        sh "aws s3 --region $region cp SampleApp_Linux.zip s3://double-digit-devl/ --acl public-read"
+                        sh "echo Finished create/update successfully!"
+                    }
+                }
+            }
+        }
         stage('lambda-infra'){
             steps {
                 dir('cloudformation/lambda/'){
@@ -85,6 +152,7 @@ pipeline {
                         def region = params.REGION
                         def stackName = params.LAMBDASTACK
                         def vpc = params.VPCSTACK
+                        def s3 = params.S3STACK
                         try {
                             status = sh(script: "aws cloudformation describe-stacks --region ${params.REGION} \
                                 --stack-name ${params.LAMBDASTACK} --query Stacks[0].StackStatus --output text", returnStdout: true)
@@ -92,14 +160,14 @@ pipeline {
                                 sh "aws cloudformation delete-stack --stack-name ${params.LAMBDASTACK} --region ${params.REGION}"
                                 sh 'echo Waiting for stack to delete....'
                                 sh "aws cloudformation --region ${params.REGION} wait stack-delete-complete --stack-name ${params.LAMBDASTACK}"
-                                sh 'echo Creating ASG group and configuration for web application after deleting....'
-                                createLambdaStack(region, stackName, vpc)
+                                sh 'echo Creating Lambda, S3, SQS for serverless computing after deleting....'
+                                createLambdaStack(region, stackName, vpc, s3)
                             }
                             apply = true
                         } catch(err){
                             apply = false
                             sh 'echo Creating Lambda infra for serverless application for first time....'
-                            createLambdaStack(region, stackName, vpc)
+                            createLambdaStack(region, stackName, vpc, s3)
                         }
                         if(apply){
                             try {
